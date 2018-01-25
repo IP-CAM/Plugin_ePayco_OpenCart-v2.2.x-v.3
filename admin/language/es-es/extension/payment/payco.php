652e22ac059b68bcf4d96bab6fda06f32d5d0c18
JsonDev <desarrollo@epayco.co>
2018-01-25 12:05:30 -0500

    Updated 25/01/2018

--- admin/language/es-es/extension/payment/payco.php
+++ admin/language/es-es/extension/payment/payco.php
@@ -28,14 +28,15 @@ $_['entry_valor_comision']='Valor Comisión Payco:';
 $_['entry_callback']     = 'URL De Respuesta:';
 $_['entry_callback_description']     = 'Url de respuesta para confirmar los pagos desde ePayco';
 $_['entry_confirmation']     = 'URL de confirmación:';
-$_['entry_confirmation_description']     = 'Url of the store where ePayco confirms the payment';
+$_['entry_confirmation_description']     = 'Url de la tienda donde ePayco confirma el pago';
 $_['entry_test']         = 'Modo Pruebas:';
 $_['entry_test_description']     = 'Habilitar el envio de pago en modo pruebas';
-$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">El total del pedido debe alcanzarse antes de que este método de pago se active</span>';
 $_['entry_initial_order_status'] = 'Estado Inicial del Pedido:';
 $_['entry_initial_order_status_description'] = 'Seleccione el estado del pedido que se aplicará a la hora de iniciar el pago de la orden';
 $_['entry_final_order_status'] = 'Estado Final del Pedido:';
 $_['entry_final_order_status_description'] = 'Seleccione el estado del pedido que se aplicará a la hora de aceptar y confirmar el pago de la orden';
+$_['entry_checkout_type_description'] = '(Onpage Checkout, el usuario a pagar permanece en el sitio) o (Standart Checkout, el usuario a pagar se redirige a la puerta de enlace de ePayco)'
 $_['entry_geo_zone']     = 'Geo Zone:';
 $_['entry_status']       = 'Habilitar/Deshabilitar:';
 $_['entry_status_description'] = 'Habilitar ePayco Checkout' ; 
