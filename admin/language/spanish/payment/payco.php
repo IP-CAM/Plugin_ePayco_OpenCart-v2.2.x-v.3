4bfbf137fa651631118ee9ef035c27603b88fa17
JsonDev <desarrollo@epayco.co>
2018-01-29 11:20:50 -0500

    Updated 29/01/2018

--- admin/language/spanish/payment/payco.php
+++ admin/language/spanish/payment/payco.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']      = 'ePayci';
+$_['heading_title']      = 'ePayco';
 $_['text_payco']					= '<a href="https://epayco.co/" target="_blank"><img src="https://369969691f476073508a-60bf0867add971908d4f26a64519c2aa.ssl.cf5.rackcdn.com/logos/logo_epayco_200px.png" alt="ePayco" title="ePayco" style="max-width: 90px;" /></a>';
 
 // Text 
